d7de0e82553b922b6e604d5d4f177f9cd9239bba
Sahil <sahilnegi.pang@gmail.com>
2024-01-17 15:05:04 +0530

    added comments

--- server.js
+++ server.js
@@ -1,79 +1,86 @@
 const http = require("http");
-const path = require("path");
 const url = require("url");
 const uuid4 = require("uuid4");
+
 const server = http.createServer((req, res) => {
+  // Parse the request URL
   const parsedUrl = url.parse(req.url);
   let pathName = parsedUrl.pathname;
-  if (pathName == "GET/html" && req.method == "GET") {
+
+  // Handle different endpoints based on the request path
+  if (pathName === "/GET/html" && req.method === "GET") {
+    // Respond with an HTML page
     res.writeHead(200, {
       "Content-Type": "text/html",
     });
-    res.statusCode = 200;
     res.end(`<!DOCTYPE html>
-        <html>
-          <head>
-          </head>
-          <body>
-              <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
-              <p> - Martin Fowler</p>
-        
-          </body>
-        </html>`);
-  } else if (pathName == "GET/json" && req.method == "GET") {
-    {
-      res.writeHead(200, {
-        "Content-Type": "text/json",
-      });
-      res.statusCode = 200;
-      res.end(`"slideshow": {
-          "author": "Yours Truly",
-          "date": "date of publication",
-          "slides": [
-            {
-              "title": "Wake up to WonderWidgets!",
-              "type": "all"
-            },
-            {
-              "items": [
-                "Why <em>WonderWidgets</em> are great",
-                "Who <em>buys</em> WonderWidgets"
-              ],
-              "title": "Overview",
-              "type": "all"
-            }
-          ],
-          "title": "Sample Slide Show"
-        }`);
-    }
-  } else if (parsedUrl.pathname == "GET/uuid" && req.method == "GET") {
+      <html>
+        <head>
+        </head>
+        <body>
+            <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
+            <p> - Martin Fowler</p>
+        </body>
+      </html>`);
+  } else if (pathName === "/GET/json" && req.method === "GET") {
+    // Respond with JSON data
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(`{
+      "slideshow": {
+        "author": "Yours Truly",
+        "date": "date of publication",
+        "slides": [
+          {
+            "title": "Wake up to WonderWidgets!",
+            "type": "all"
+          },
+          {
+            "items": [
+              "Why <em>WonderWidgets</em> are great",
+              "Who <em>buys</em> WonderWidgets"
+            ],
+            "title": "Overview",
+            "type": "all"
+          }
+        ],
+        "title": "Sample Slide Show"
+      }
+    }`);
+  } else if (pathName === "/GET/uuid" && req.method === "GET") {
+    // Respond with a generated UUID
     let getUuid = uuid4();
     res.writeHead(200, {
-      "Content-Type": "text/json",
+      "Content-Type": "application/json",
     });
-    res.statusCode = 200;
-    console.log("uuid", getUuid);
     res.end(`{
-      uuid: ${getUuid},
-}`);
-  } else if (parsedUrl.pathname.includes("GET/status") && req.method == "GET") {
-    let status = parsedUrl.pathname.split("/")[2];
+      "uuid": "${getUuid}"
+    }`);
+  } else if (pathName.includes("/GET/status") && req.method === "GET") {
+    // Extract the status code from the URL and respond with a message
+    let status = pathName.split("/")[2];
     res.writeHead(200, {
       "Content-Type": "text/plain",
     });
-    res.statusCode = 200;
     res.end(` ${status} status code .`);
-  } else if (parsedUrl.pathname.includes("GET/delay") && req.method == "GET") {
-    let delay = Number(parsedUrl.pathname.split("/")[2]);
-    console.log(delay);
+  } else if (pathName.includes("/GET/delay") && req.method === "GET") {
+    // Extract the delay time from the URL and respond after the specified delay
+    let delay = Number(pathName.split("/")[2]);
     res.writeHead(200, {
       "Content-Type": "text/html",
     });
-    res.statusCode = 200;
     setTimeout(() => {
       res.end(`  <h1>200 Status Code</h1>`);
     }, delay * 1000);
+  } else {
+    // Handle 404 for unknown endpoints
+    res.writeHead(404, {
+      "Content-Type": "text/plain",
+    });
+    res.end('Not Found');
   }
+  
 });
 
 const port = 8000;
